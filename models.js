5dc6b4b8a1f58670abce753f8c290811333a644c
James Guthrie <jamesrguthrie@gmail.com>
2017-05-09 17:42:06 -0400

    added get and post endpoings for user

--- models.js
+++ models.js
@@ -1,19 +1,19 @@
 const mongoose = require('mongoose')
 
-const userSchema = new mongoose.Schema( {
+mongoose.Promise = global.Promise;
+
+const userSchema = mongoose.Schema({
   name: {type: String, required: true},
   email: {type: String, required: true},
   password: {type: String, required: true},
 });
 
-userSchema.methods.apiRepr = () => {
+userSchema.methods.apiRepr = function() {
   return {
     name: this.name,
     email: this.email,
     id: this._id,
-  }
-}
+  };
+};
 
-const User = mongoose.model('User', userSchema);
-
-module.exports = { User };
+module.exports = mongoose.model('User', userSchema)
--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const bodyParser = require('body-parser');
 const express = require('express');
 const mongoose = require('mongoose');
 const morgan = require('morgan');
-const User = require('./models');
 
 const {DATABASE_URL, PORT} = require('./config');
 
@@ -11,71 +10,99 @@ const app = express();
 app.use(morgan('common'));
 app.use(bodyParser.json());
 
+const User = require('./models');
+
 mongoose.Promise = global.Promise;
 
-app.post('api/users', (req, res) => {
-  if (!req.body) {
-    return res.status(400).json({message: 'No request body'});
-  }
+app.get('/', (req, res) => {
+  console.log('hello');
+})
 
-  if (!('name' in req.body)) {
-    return res.status(422).json({message: 'Missing field: name'});
-  }
+app.get('/api/users', (req, res) => {
+  console.log('hello');
+  return User.find({})
+  .then(users => {
+    return res.status(201).json(users);
+  })
+})
 
-  let {name, email, password} = req.body;
+app.post('/api/users', (req, res) => {
+  console.log(req.body);
 
-  if (typeof name !== 'string') {
-    return res.status(422).json({message: 'Empty field type: name'});
-  }
+  const {email, password, name} = req.body;
+  console.log(email);
 
-  name = name.trim();
-
-  if (name === '') {
-    return res.status(422).json({message: 'Empty field length: name'});
-  }
-
-  email = email.trim();
-
-  if (email === '') {
-    return res.status(422).json({message: 'Empty field length: email'})
-  }
-
-  if (!(password)) {
-    return res.status(422).json({message: 'Missing field: password'});
-  }
-
-  if (typeof password !== 'string') {
-    return res.status(422).json({message: 'Incorrect field type: password'});
-  }
-
-  password = password.trim();
-
-  if (password === '') {
-    return res.status(422).json({message: 'Incorrect field length: password'});
-  }
+  return User
+    .create({
+      name,
+      email,
+      password
+    })
+  .then(user => {
+    return (res.status(201).json(user.apiRepr()));
+  })
+  // if (!req.body) {
+  //   return res.status(400).json({message: 'No request body'});
+  // }
+  //
+  // if (!('name' in req.body)) {
+  //   return res.status(422).json({message: 'Missing field: name'});
+  // }
+  //
+  // let {name, email, password} = req.body;
+  //
+  // if (typeof name !== 'string') {
+  //   return res.status(422).json({message: 'Empty field type: name'});
+  // }
+  //
+  // name = name.trim();
+  //
+  // if (name === '') {
+  //   return res.status(422).json({message: 'Empty field length: name'});
+  // }
+  //
+  // email = email.trim();
+  //
+  // if (email === '') {
+  //   return res.status(422).json({message: 'Empty field length: email'})
+  // }
+  //
+  // if (!(password)) {
+  //   return res.status(422).json({message: 'Missing field: password'});
+  // }
+  //
+  // if (typeof password !== 'string') {
+  //   return res.status(422).json({message: 'Incorrect field type: password'});
+  // }
+  //
+  // password = password.trim();
+  //
+  // if (password === '') {
+  //   return res.status(422).json({message: 'Incorrect field length: password'});
+  // }
 
   // check for existing user
-  return User
-    .find({email})
-    .count()
-    .exec()
-    .then(count => {
-      if (count > 0) {
-        return res.status(422).json({message: 'email already taken'});
-      }
-      return User
-        .create({
-          name: name,
-          password: password,
-          email: email,
-        })
-    })
-    .then(user => {
-      return res.status(201).json(user.apiRepr());
-    })
-    .catch(err => {
-      res.status(500).json({message: 'Internal server error'})
-    });
+
+  // return User
+  //   .find({email})
+  //   .count()
+  //   .then(count => {
+  //     if (count > 0) {
+  //       return res.status(422).json({message: 'email already taken'});
+  //     }
+  //     return User
+  //       .create({
+  //         name: name,
+  //         password: password,
+  //         email: email,
+  //       })
+  //   })
+  //   .then(user => {
+  //     return res.status(201).json(user.apiRepr());
+  //   })
+  //   .catch(err => {
+  //     res.status(500).json({message: 'Internal server error'})
+  //   });
 })
 
 // closeServer needs access to a server object, but that only
